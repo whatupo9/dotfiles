48c6b57ff204e29cc72d179835029cd99e39ef79
whatupo9 <cameron.macg@icloud.com>
2025-07-01 12:32:04 -0400

    Remapped accept completion to C-space

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -27,9 +27,8 @@ return {
                 mapping = cmp.mapping.preset.insert({
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<C-Tab>"] = function()
+                    ["<C-Space>"] = function()
                         cmp.confirm({ select = true })
                     end,
                 }),
