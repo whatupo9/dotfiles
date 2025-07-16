ce6ea240f704ffda21092bb6444c573740de0eb2
whatupo9 <cameron.macg@icloud.com>
2025-07-16 19:43:20 -0400

    Fixed some lsp stuff

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -19,14 +19,6 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			capabilities.offsetEncoding = { "utf-8" }
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,15 +1,17 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.cppcheck,
-				null_ls.builtins.formatting.clang_format.with({
-            extra_args = { "--style=file" },
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.cppcheck.with({
+          extra_args = { "--language=c++", "--force" },
         }),
-			},
-		})
-	end,
+        null_ls.builtins.formatting.clang_format.with({
+          extra_args = { "--style=file" },
+        }),
+      },
+    })
+  end,
 }
