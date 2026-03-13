40f5f8ce11a18c2e87355af00422fb9cc47be96f
Cameron MacGillivary <cameron.macg@icloud.com>
2026-03-13 09:40:27 -0400

    Disable auto_install for nvim-treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,13 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+  branch = "master",
 	build = ":TSUpdate",
 	config = function()
+    require("nvim-treesitter.install").prefer_git = true;
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-			auto_install = true,
+      ensure_installed = {c, cpp, lua, bash, vim, latex},
+      auto_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
